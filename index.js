f74116fa8752fd5a3ca0a50914cd76d9ff00d711
joliver427@icloud.com <joliver427@icloud.com>
2023-05-18 13:26:49 -0600

    commit tree eb16821d9144e0874e27afc75d07f3c71cc702d2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jeremy",
+    streedAddress: "6111 W Main St."
+}
+
+function updateEmployeeWithKeyAndValue(Obj, key, value) {
+    const newEmployee = {...Obj, ...{ [key]: value } }
+    return newEmployee
+}
